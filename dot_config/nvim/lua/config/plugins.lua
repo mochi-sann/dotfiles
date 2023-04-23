04c316f36c74b64c97fcb62d439a6f9155de1706
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2023-04-23 15:02:27 +0900

    update settings

--- dot_config/nvim/lua/config/plugins.lua
+++ dot_config/nvim/lua/config/plugins.lua
@@ -531,6 +531,19 @@ return require("lazy").setup({ -- Packer can manage itself
 			require("plugconfig/nvim-tree")
 		end,
 	},
+	{
+		"stevearc/aerial.nvim",
+		config = function()
+			vim.keymap.set("n", "<leader>a", "<cmd>AerialToggle!<CR>")
+			require("aerial").setup({
+				on_attach = function(bufnr)
+					-- Jump forwards/backwards with '{' and '}'
+					vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+					vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
+				end,
+			})
+		end,
+	},
 
 	-- {
 	--   dir = "~/codespace/github.com/mochi-sann/Selected2Browser.nvim",
