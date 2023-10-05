f8d2afc7af8553deca8554c7ea98158dac16f318
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2023-10-05 22:01:48 +0900

    update

--- dot_config/nvim/lua/config/plugins.lua
+++ dot_config/nvim/lua/config/plugins.lua
@@ -331,7 +331,8 @@ return require("lazy").setup({ -- Packer can manage itself
 	-- },
 	{
 		"lukas-reineke/indent-blankline.nvim",
-		event = "VimEnter",
+		main = "ibl",
+    opts = { },
 		config = function()
 			require("plugconfig/indent_blankline")
 		end,
--- dot_config/nvim/lua/plugconfig/indent_blankline.lua
+++ dot_config/nvim/lua/plugconfig/indent_blankline.lua
@@ -1,44 +1,30 @@
 vim.opt.list = true
 vim.opt.listchars:append("space:⋅")
--- vim.opt.listchars:append("eol:↴")
+local highlight = {
+	"RainbowRed",
+	"RainbowYellow",
+	"RainbowBlue",
+	"RainbowOrange",
+	"RainbowGreen",
+	"RainbowViolet",
+	"RainbowCyan",
+}
 
-vim.cmd([[highlight IndentBlanklineIndent1 guifg=#a05059 gui=nocombine]])
-vim.cmd([[highlight IndentBlanklineIndent2 guifg=#a48a5d gui=nocombine]])
-vim.cmd([[highlight IndentBlanklineIndent3 guifg=#6e8c5c gui=nocombine]])
-vim.cmd([[highlight IndentBlanklineIndent4 guifg=#41838d gui=nocombine]])
-vim.cmd([[highlight IndentBlanklineIndent5 guifg=#487eac gui=nocombine]])
-vim.cmd([[highlight IndentBlanklineIndent6 guifg=#8e58a0 gui=nocombine]])
+local hooks = require("ibl.hooks")
+-- create the highlight groups in the highlight setup hook, so they are reset
+-- every time the colorscheme changes
+hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+	vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#913f45" })
+	vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#7c6741" })
+	vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#2d506f" })
+	vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#9e744d" })
+	vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#789a60" })
+	vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#8d559d" })
+	vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#45929c" })
+end)
 
-require("indent_blankline").setup({
-	-- space_char_blankline = "",
-	-- show_current_context = true,
-	-- show_current_context_start = true,
-	char_highlight_list = {
-		"IndentBlanklineIndent1",
-		"IndentBlanklineIndent2",
-		"IndentBlanklineIndent3",
-		"IndentBlanklineIndent4",
-		"IndentBlanklineIndent5",
-		"IndentBlanklineIndent6",
-	},
-	filetype_exclude = {
-		"startify",
-		"dashboard",
-		"dotooagenda",
-		"fugitive",
-		"gitcommit",
-		"packer",
-		"vimwiki",
-		"markdown",
-		"txt",
-		"vista",
-		"help",
-		"todoist",
-		"NvimTree",
-		"peekaboo",
-		"git",
-		"TelescopePrompt",
-		"undotree",
-		"flutterToolsOutline",
+require("ibl").setup({
+	indent = {
+		highlight = highlight,
 	},
 })
