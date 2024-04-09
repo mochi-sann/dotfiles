c1109d4c6128d95fde8005390d9e9aea00a1728d
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2024-04-09 11:40:42 +0900

    update

---
+++ dot_config/atcoder-cli-nodejs/python/main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3 
+import sys
+import math
+import itertools
+import collections
+import heapq
+import bisect
+import re
+
+def main():
+    s = input()
+    print(s)
+
+if __name__ == '__main__':
+    main()
