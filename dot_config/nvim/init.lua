46768848200178853f4b4aa80f81fbceadde4d3f
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2024-06-22 11:32:55 +0900

    update

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -9,5 +9,9 @@ autocmd BufRead,BufEnter *.slim set filetype=slim
 nmap <silent> <Esc><Esc> :nohlsearch<CR><Esc>
 tnoremap <Esc> <C-\><C-n>
 colorschem tokyonight
+set termguicolors
+let &t_8f = "\<Esc>[38;2;%lu;%lu;%lum"
+let &t_8b = "\<Esc>[48;2;%lu;%lu;%lum"
+
 " hi Normal ctermbg=NONE ctermfg=252 guibg=#NONE guifg=#c6c8d1
 ]])
