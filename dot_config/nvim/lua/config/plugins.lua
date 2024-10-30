27c3d05d9575536009d72683f89806f48b87f70c
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2024-10-30 11:14:44 +0900

    add ai for nivm

--- dot_config/nvim/lua/config/plugins.lua
+++ dot_config/nvim/lua/config/plugins.lua
@@ -887,4 +887,81 @@ return require("lazy").setup({ -- Packer can manage itself
 			port = 24917,
 		},
 	},
+	{
+		"yetone/avante.nvim",
+		event = "VeryLazy",
+		lazy = false,
+		version = false, -- set this if you want to always pull the latest change
+		opts = {
+			---@alias Provider "claude" | "openai" | "azure" | "gemini" | "cohere" | "copilot" | string
+			provider = "Lalama-3.1", -- Recommend using Claude
+			vendors = {
+				["Lalama-3.1"] = {
+					endpoint = "http://192.168.3.4:1234/v1",
+					model = "lmstudio-community/Meta-Llama-3.1-8B-Instruct-GGUF",
+					timeout = 1000 * 30, -- Timeout in milliseconds
+					temperature = 0.7,
+					max_tokens = -1,
+					["local"] = true,
+					parse_curl_args = function(opts, code_opts)
+						return {
+							url = opts.endpoint .. "/chat/completions",
+							headers = {
+								["Accept"] = "application/json",
+								["Content-Type"] = "application/json",
+							},
+							body = {
+								model = opts.model,
+								messages = require("avante.providers").copilot.parse_message(code_opts), -- you can make your own message, but this is very advanced
+								max_tokens = 2048,
+								stream = true,
+							},
+						}
+					end,
+					parse_response_data = function(data_stream, event_state, opts)
+						require("avante.providers").openai.parse_response(data_stream, event_state, opts)
+					end,
+				},
+			},
+
+			-- add any opts here
+		},
+		-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+		build = "make",
+		-- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"stevearc/dressing.nvim",
+			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+			--- The below dependencies are optional,
+			"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+			"zbirenbaum/copilot.lua", -- for providers='copilot'
+			{
+				-- support for image pasting
+				"HakonHarnes/img-clip.nvim",
+				event = "VeryLazy",
+				opts = {
+					-- recommended settings
+					default = {
+						embed_image_as_base64 = false,
+						prompt_for_file_name = false,
+						drag_and_drop = {
+							insert_mode = true,
+						},
+						-- required for Windows users
+						use_absolute_path = true,
+					},
+				},
+			},
+			{
+				-- Make sure to set this up properly if you have lazy=true
+				"MeanderingProgrammer/render-markdown.nvim",
+				opts = {
+					file_types = { "markdown", "Avante" },
+				},
+				ft = { "markdown", "Avante" },
+			},
+		},
+	},
 })
