567117c5826979ffd7010f11e3e42a8078a1f5cc
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2024-11-14 17:21:13 +0900

    update

--- dot_config/nvim/lua/config/plugins.lua
+++ dot_config/nvim/lua/config/plugins.lua
@@ -862,6 +862,17 @@ return require("lazy").setup({ -- Packer can manage itself
 			require("plugconfig/spectre")
 		end,
 	},
+    {
+    'MagicDuck/grug-far.nvim',
+    config = function()
+      require('grug-far').setup({
+
+        -- options, see Configuration section below
+        -- there are no required options atm
+        -- engine = 'ripgrep' is default, but 'astgrep' can be specified
+      });
+    end
+  },
 	{
 		"cshuaimin/ssr.nvim",
 		event = "VeryLazy",
