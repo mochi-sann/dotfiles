13ee8af96298443ef81d5f6ad5f0af6ac3d5b41b
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2025-01-08 13:41:02 +0900

    update

--- dot_config/nvim/lua/plugins/nvim-cmp.lua
+++ dot_config/nvim/lua/plugins/nvim-cmp.lua
@@ -1,6 +1,6 @@
 return {
 	"hrsh7th/nvim-cmp",
-  event = { "InsertEnter", "CmdwinEnter", "CmdlineEnter" },
+	event = { "InsertEnter", "CmdwinEnter", "CmdlineEnter" },
 	dependencies = {
 		{ "hrsh7th/cmp-buffer" },
 		{ "hrsh7th/cmp-emoji" },
