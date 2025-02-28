3c6dba72a4e9d0d7311926c07d8c4e3b2cd4367c
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2025-02-28 11:25:58 +0900

    update

--- dot_config/nvim/lua/config/plugins.lua
+++ dot_config/nvim/lua/config/plugins.lua
@@ -511,21 +511,21 @@ return require("lazy").setup({ -- Packer can manage itself
 
 		event = "VeryLazy",
 	},
-	{
-		"stevearc/aerial.nvim",
-		event = "VeryLazy",
-		config = function()
-			vim.keymap.set("n", "<leader>a", "<cmd>AerialToggle!<CR>")
-
-			require("aerial").setup({
-				on_attach = function(bufnr)
-					-- Jump forwards/backwards with '{' and '}'
-					vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
-					vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
-				end,
-			})
-		end,
-	},
+	-- {
+	-- 	"stevearc/aerial.nvim",
+	-- 	event = "VeryLazy",
+	-- 	config = function()
+	-- 		vim.keymap.set("n", "<leader>a", "<cmd>AerialToggle!<CR>")
+	--
+	-- 		require("aerial").setup({
+	-- 			on_attach = function(bufnr)
+	-- 				-- Jump forwards/backwards with '{' and '}'
+	-- 				vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+	-- 				vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
+	-- 			end,
+	-- 		})
+	-- 	end,
+	-- },
 
 	{ "tikhomirov/vim-glsl", event = "VeryLazy" },
 	{ "koron/vim-budoux", event = "VeryLazy" },
