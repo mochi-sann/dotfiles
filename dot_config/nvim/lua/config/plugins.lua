060de458f6320553463a3df6c08589bc020a1ac6
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2025-04-09 19:05:06 +0900

    feat(plugins): vim-deviconsを遅延読み込みに設定 feat(hop): Hopプラグインのキーマッピングを整理 feat(git-bleame): git-bleameプラグインにコマンドと遅延読み込みを追加

--- dot_config/nvim/lua/config/plugins.lua
+++ dot_config/nvim/lua/config/plugins.lua
@@ -338,7 +338,7 @@ return require("lazy").setup({ -- Packer can manage itself
 			require("plugconfig/whichi_key")
 		end,
 	},
-	{ "ryanoasis/vim-devicons" }, -- {
+	{ "ryanoasis/vim-devicons", lazy = true }, -- {
 	-- 	"relastle/vim-colorrange",
 	-- 	event = "VeryLazy",
 	-- },
--- dot_config/nvim/lua/plugins/git-bleame.lua
+++ dot_config/nvim/lua/plugins/git-bleame.lua
@@ -1,5 +1,7 @@
 return {
 	"FabijanZulj/blame.nvim",
+	cmd = { "BlameToggle", "BlameOpen" },
+	lazy = true,
 	config = function()
 		require("blame").setup({})
 	end,
