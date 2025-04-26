e3ef0f7e1e660e83aa3cea5f43f05f43887d9c36
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2025-04-26 20:00:21 +0900

    スタイル(octo.lua): インデントをタブからスペースに変更

--- dot_config/nvim/lua/plugins/octo.lua
+++ dot_config/nvim/lua/plugins/octo.lua
@@ -1,12 +1,12 @@
 return {
-  "pwntester/octo.nvim",
-  lazy = true,
-  cmd = { "Octo" },
-  event = "VeryLazy",
-  keys = {
-    { "<leader>gh", "<cmd>Octo<cr>", desc = "GitHub" },
-  },
-  config = function()
-    require "octo".setup()
-  end,
+	"pwntester/octo.nvim",
+	lazy = true,
+	cmd = { "Octo" },
+	event = "VeryLazy",
+	keys = {
+		{ "<leader>gh", "<cmd>Octo<cr>", desc = "GitHub" },
+	},
+	config = function()
+		require("octo").setup()
+	end,
 }
