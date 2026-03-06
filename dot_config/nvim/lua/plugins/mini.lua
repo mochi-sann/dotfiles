6c451f14f43621d53806ab66dd7d75e2bb871d19
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2026-03-06 18:07:03 +0900

    mini.luaをアップデート

--- dot_config/nvim/lua/plugins/mini.lua
+++ dot_config/nvim/lua/plugins/mini.lua
@@ -27,6 +27,28 @@ return {
 				["`"] = { action = "closeopen", pair = "``", neigh_pattern = "[^\\].", register = { cr = false } },
 			},
 		})
+
+		require("mini.surround").setup({
+
+			-- Number of lines within which surrounding is searched
+
+			-- Duration (in ms) of highlight when calling `MiniSurround.highlight()`
+
+			-- Pattern to match function name in 'function call' surrounding
+			-- By default it is a string of letters, '_' or '.'
+			-- funname_pattern = "[%w_%.]+",
+
+			-- Module mappings. Use `''` (empty string) to disable one.
+			mappings = {
+				add = "sa", -- Add surrounding
+				delete = "sd", -- Delete surrounding
+				find = "sf", -- Find surrounding (to the right)
+				find_left = "sF", -- Find surrounding (to the left)
+				highlight = "sh", -- Highlight surrounding
+				replace = "sr", -- Replace surrounding
+				update_n_lines = "sn", -- Update `n_lines`
+			},
+		})
 		-- require('mini.sessions').setup({
 		--
 		-- })
